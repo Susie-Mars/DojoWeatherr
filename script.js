435798e81e0e4300bc73b9e617a32d7a5d685279
Susie-Mars <marsnarsbars@gmail.com>
2022-09-24 15:36:16 -0600

    Dojo Weather

---
+++ script.js
@@ -0,0 +1,37 @@
+function myFunction() {
+    alert("Loading weather report");
+}
+
+function hide(element) {
+    document.querySelector(element).remove();
+}
+
+// function changeTemp(element) {
+//     document.querySelector(element).innerText = "75°";
+// }
+
+function changeTemp("#high1") {
+    document.getElementById("high1").innerText = "75°";
+}
+function changeTemp() {
+    document.getElementById("low1").innerText = "65°";
+}
+function changeTemp() {
+    document.getElementById("high2").innerText = "80°";
+}
+function changeTemp() {
+    document.getElementById("low2").innerText = "66°";
+}
+function changeTemp() {
+    document.getElementById("high3").innerText = "69°";
+}
+function changeTemp() {
+    document.getElementById("low3").innerText = "61°";
+}
+function changeTemp() {
+    document.getElementById("high4").innerText = "78°";
+}
+function changeTemp() {
+    document.getElementById("low4").innerText = "70°";
+}
+
